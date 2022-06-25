cdc27916d91932c7253240ee7b8c3cb841daa65d
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-06-25 17:04:16 -0300

    rebase to code in file

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-     <h1 className="text-5xl font-bold text-violet-500">Hello World!</h1>
+     <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
   )
 }
