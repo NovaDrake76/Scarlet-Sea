7d5e23a64d4226f0f69a720a72a27b1bb399f48c
Nathan <novadrake76@gmail.com>
2022-06-26 14:51:57 -0300

    fix: tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-
 export default function Home() {
   return (
     <div>
-     <h1 className="text-large">Hello World!</h1>
-         <button>
-            Save changes
-         </button>
-   </div>
+      <h1 className="text-3xl font-bold underline">Scarlet Sea</h1>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   purge: {
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}',
-    ]
-   },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
