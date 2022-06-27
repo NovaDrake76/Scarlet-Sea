94dd5eaea0839536d773241e4baffc6987de6336
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-06-27 18:53:44 -0300

    finished footer, add to icons now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -168,12 +168,11 @@ export default function Home() {
 
       </div>
          <ItemsContainer/>
-         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-10
+         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
          text-center pt-2 text-gray-400 text-sm pb-8"
          >
           <span>@ 2022 Appy. All rights reserved</span>
           <span>Terms - Privacy Policy</span>
-           <FaAws/> <FaPaypal/> 
          </div>
     </footer>
     </>
