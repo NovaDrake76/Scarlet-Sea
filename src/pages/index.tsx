2313b49941feacc7706065bd71d3f3d4b2197435
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-06-27 10:49:25 -0300

    add to footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { FaDiscord, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
 
 export default function Home() {
   return (
+    <>
     <div className="flex flex-col ">
       <div className="flex flex-col items-center justify-end h-screen bg-center bg-no-repeat bg-cover md:justify-center md:items-start bg-sdBackground md:bg-background">
         <div className="flex flex-col md:ml-28">
@@ -137,5 +138,22 @@ export default function Home() {
         </div>
       </div>
     </div>
+
+    <footer className="bg-gray-900 text-white">
+      <div className=" md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-3-normal font-semibold 
+        md:w-2/5"
+        >
+          <span className="text-teal-400">Free</span> Until you're ready to 
+          launch 
+          </h1>
+          <div>
+            <input type="text" placeholder="Enter Your ph.no" className="text-gray-800
+            sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"/>
+
+          </div>
+      </div>
+    </footer>
+    </>
   )
 }
