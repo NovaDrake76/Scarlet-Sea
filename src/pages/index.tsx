c4aea4f67a89329de124452600d2cd633c837992
Nathan <novadrake76@gmail.com>
2022-06-28 10:21:09 -0300

    fix: navbar color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import Counter from "../components/counter"
-import { FaAppStore, FaDiscord, FaFacebook, FaGooglePlay, FaInstagram, FaTwitter, } from "react-icons/fa"
+import {
+  FaAppStore,
+  FaDiscord,
+  FaFacebook,
+  FaGooglePlay,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa"
 import ItemsContainer from "../components/ItemsContainer"
-import Image from 'next/image'
+import Image from "next/image"
 import { Navbar } from "../components/Navbar"
-
-
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className="flex flex-col ">
         <div className="flex flex-col items-center justify-end h-screen bg-center bg-no-repeat bg-cover md:justify-center md:items-start bg-sdBackground md:bg-background">
           <div className="flex flex-col md:ml-28">
@@ -29,7 +34,10 @@ export default function Home() {
         </div>
         <div className="flex flex-col justify-center align-middle bg-[#211F1F] p-12 gap-12">
           <div className="flex flex-col gap-6">
-            <h2 id="About" className="leading-normal  text-center text-red-600 text-6xl font-[cinzel] font-semibold 	">
+            <h2
+              id="About"
+              className="leading-normal text-center text-red-600 text-6xl font-[cinzel] font-semibold 	"
+            >
               EXPLORE A NEW WORLD
             </h2>
             <h4 className="text-3xl leading-normal text-center text-white ">
@@ -97,7 +105,10 @@ export default function Home() {
         </div>
         <div className="flex flex-col justify-around h-screen md:h-[70vh] py-12  px-8 bg-no-repeat bg-cover md:flex-row bg-register">
           <div className="flex flex-col">
-            <h3 id="Register" className="leading-normal text-center text-red-600 text-3xl md:text-5xl font-[cinzel] font-semibold 	">
+            <h3
+              id="Register"
+              className="leading-normal text-center text-red-600 text-3xl md:text-5xl font-[cinzel] font-semibold 	"
+            >
               PRE-REGISTER
             </h3>
             <div className="flex px-8 py-4 bg-white rounded-[4px]">
@@ -152,34 +163,25 @@ export default function Home() {
       </div>
 
       <footer id="Services" className="text-white bg-gray-900">
-        <div className=" md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-          <h1 className="mb-6 text-3xl font-semibold lg:text-4xl md:mb-0 lg:leading-3-normal md:w-2/5">
-            <span className="text-teal-400">Free</span> Until you&apos;re ready
-            to invest
-          </h1>
-          
-          <div>
-            <input type="text" placeholder="Enter Your ph.no" className="text-gray-800
-            sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
-          />
-          <button className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
-          rounded-md text-white md:w-auto w-full">
-            Request Code
-          </button>
-          </div>
-
-      </div>
-         <ItemsContainer/>
-         <div className="grid grid-cols-1 gap-10 pt-2 pb-8 text-sm text-center text-gray-400 sm:grid-cols-3 lg:grid-cols-3"
-         >
+        <ItemsContainer />
+        <div className="grid grid-cols-1 gap-10 pt-2 pb-8 text-sm text-center text-gray-400 sm:grid-cols-3 lg:grid-cols-3">
           <span>@ 2022 Appy. All rights reserved</span>
           <span>Terms - Privacy Policy</span>
-          <span className=" flex md:justify-center md:items-center justify-center gap-5 md:flex-row
-          "><FaGooglePlay/><FaAppStore/> 
+          <span
+            className=" flex md:justify-center md:items-center justify-center gap-5 md:flex-row
+          "
+          >
+            {/* <FaGooglePlay />
+            <FaAppStore /> */}
+            <Image
+              src="/img/nvidia.png"
+              alt="placeholder"
+              width={500}
+              height={500}
+            ></Image>
           </span>
-         
-         </div> 
-    </footer>
+        </div>
+      </footer>
     </>
   )
 }
