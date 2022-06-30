c8a3c934695dd63c25515f6154bb69a6a5620601
Nathan <novadrake76@gmail.com>
2022-06-29 22:30:26 -0300

    fix netlify anchor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link"
 
 export function Navbar() {
-
   return (
     <>
       <nav className="bg-gray-800 w-full flex justify-between px-5 md:px-10 py-4 items-center">
         <div>
-            <div className="flex gap-4 md:gap-10 ">
+          <div className="flex gap-4 md:gap-10 ">
             <Link href="/">
               <a className="text-sm font-medium text-white rounded-md hover:bg-gray-700">
                 HOME
@@ -18,19 +17,17 @@ export function Navbar() {
                 ABOUT
               </a>
             </Link>
-            <a
-              target="_blank"
+            <Link
               rel="noreferrer"
+              target="_blank"
               href="https://docs.google.com/document/d/1XT0fQcI7XrKEfZeERzsnI0JtlY5jqGZVABEx3VUIeC0/edit"
-              className=" text-sm font-medium text-white rounded-md hover:bg-gray-700 hover:text-white"
-            >
-            GAME DESIGN
-            </a>
-            </div>
-
+            ></Link>
+          </div>
         </div>
         <div className="text-center hidden md:flex">
-          <a href="/#register" className="text-red-600">PRE-REGISTER NOW</a>
+          <a href="/#register" className="text-red-600">
+            PRE-REGISTER NOW
+          </a>
         </div>
       </nav>
     </>
