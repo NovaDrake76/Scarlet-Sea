2454e81714d8486ba2cf6ea56765f48622f597ac
Nathan <novadrake76@gmail.com>
2022-06-29 22:41:25 -0300

    fix: navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export function Navbar() {
   return (
     <>
-      <nav className="bg-gray-800 w-full flex justify-between px-5 md:px-10 py-4 items-center">
+      <nav className="flex items-center justify-between w-full px-5 py-4 bg-gray-800 md:px-10">
         <div>
           <div className="flex gap-4 md:gap-10 ">
             <Link href="/">
@@ -13,7 +13,7 @@ export function Navbar() {
             </Link>
 
             <Link href="about">
-              <a className=" text-sm font-medium text-white rounded-md hover:bg-gray-700 ">
+              <a className="text-sm font-medium text-white rounded-md  hover:bg-gray-700">
                 ABOUT
               </a>
             </Link>
@@ -22,11 +22,13 @@ export function Navbar() {
               target="_blank"
               href="https://docs.google.com/document/d/1XT0fQcI7XrKEfZeERzsnI0JtlY5jqGZVABEx3VUIeC0/edit"
             >
-              <a>GAME DESIGN</a>
+              <a className="text-sm font-medium text-white rounded-md  hover:bg-gray-700">
+                GAME DESIGN
+              </a>
             </Link>
           </div>
         </div>
-        <div className="text-center hidden md:flex">
+        <div className="hidden text-center md:flex">
           <Link href="/#register">
             <a className="text-red-600">PRE-REGISTER NOW</a>
           </Link>
